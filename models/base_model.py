067b2a4f4e2ae78ce3eae4f5a7b7922ba0a03551
godwinn22 <godwinohazulike@gmail.com>
2024-03-11 00:55:30 +0100

    Solved task 3. BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -16,7 +16,7 @@ class BaseModel:
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
-        self.updated_at = self.created_at
+        self.updated_at = datetime.datetime.now()
 
     def __str__(self):
         """This method returns a string representation"""
@@ -36,4 +36,4 @@ class BaseModel:
         dict_rep['__class__'] = type(self).__name__
         dict_rep['created_at'] = dict_rep['created_at'].isoformat()
         dict_rep['updated_at'] = dict_rep['updated_at'].isoformat()
-        return dict_rep
+        return
